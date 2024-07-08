bbc002cd248c3b34a830d23e287073a93137fbba
harris-ahmad <24100315@lums.edu.pk>
2024-07-09 03:09:33 +0500

    complete

--- utils/sequelize.js
+++ utils/sequelize.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  String(process.env.DB_PASSWD),
+  process.env.DB_USER,
+  process.env.DB_PASSWD,
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    port: process.env.DB_PORT,
   }
 );
 
