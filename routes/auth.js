e87fe396ce17aec60b0e5685e9be06132fd6f091
harris-ahmad <24100315@lums.edu.pk>
2024-07-16 05:50:40 +0500

    converted to an npm package

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,9 @@
-// routes/auth.js
 const express = require('express');
 const router = express.Router();
-const passport = require('../utils/passport');
+const passport = require('passport'); // Correctly import passport here
 const bcrypt = require('../utils/bcrypt');
-
 const sequelize = require('../utils/sequelize');
-
 const { DataTypes } = require('sequelize');
-
 const Contractor = require('../models/contractor')(sequelize, DataTypes);
 
 router.post('/register', async (req, res, next) => {
@@ -42,11 +38,10 @@ router.post('/login', (req, res, next) => {
 });
 
 router.get('/logout', (req, res, next) => {
-  req.logout({ keepSessionInfo: false }, (err) => {
+  req.logout((err) => {
     if (err) { return next(err); }
     res.status(200).json({ message: 'Logged out successfully' });
   });
 });
-
 
 module.exports = router;
