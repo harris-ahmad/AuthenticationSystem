423fc7ade37ff5a2ea064362f0700c3ef9564df1
harris-ahmad <harrisah@buffalo.edu>
2025-11-07 10:31:24 -0500

    new models for auditing and session added

---
+++ models/refreshToken.js
@@ -0,0 +1,67 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class RefreshToken extends Model {
+    static associate(models) {
+      RefreshToken.belongsTo(models.User, { foreignKey: "userId", as: "user" });
+    }
+
+    isExpired() {
+      return Date.now() >= this.expiresAt.getTime();
+    }
+  }
+
+  RefreshToken.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      token: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      expiresAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      isRevoked: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      deviceInfo: {
+        type: DataTypes.JSONB,
+        allowNull: true,
+      },
+      ipAddress: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "RefreshToken",
+      tableName: "refresh_tokens",
+      timestamps: true,
+      indexes: [
+        { fields: ["userId"] },
+        { fields: ["token"] },
+        { fields: ["expiresAt"] },
+      ],
+    }
+  );
+
+  return RefreshToken;
+};
